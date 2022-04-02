5fbd6f7c858e7ef58fddf233bd7716d2a8f1ca51
P4R4NORM4L <belanger2008@gmail.com>
2022-04-02 04:02:32 -0600

    This is the final output!

---
+++ weapondamage.lua
@@ -0,0 +1,130 @@
+CreateThread(function()
+
+--Melee--
+SetWeaponDamageModifier(`weapon_dagger`, 0.85)
+SetWeaponDamageModifier(`weapon_bat`, 0.85)
+SetWeaponDamageModifier(`weapon_bottle`, 0.85)
+SetWeaponDamageModifier(`weapon_crowbar`, 0.75)
+SetWeaponDamageModifier(`weapon_unarmed`,0.45)
+SetWeaponDamageModifier(`weapon_flashlight`, 0.75)
+SetWeaponDamageModifier(`weapon_golfclub`, 0.85)
+SetWeaponDamageModifier(`weapon_hammer`, 0.75)
+SetWeaponDamageModifier(`weapon_hatchet`, 0.8)
+SetWeaponDamageModifier(`weapon_knuckle`, 0.7)
+SetWeaponDamageModifier(`weapon_knife`, 0.8)
+SetWeaponDamageModifier(`weapon_machete`, 0.8)
+SetWeaponDamageModifier(`weapon_switchblade`, 0.8)
+SetWeaponDamageModifier(`weapon_nightstick`, 0.75)
+SetWeaponDamageModifier(`weapon_wrench`, 0.75)
+SetWeaponDamageModifier(`weapon_battleaxe`, 0.8)
+SetWeaponDamageModifier(`weapon_poolcue`, 0.85)
+SetWeaponDamageModifier(`weapon_stone_hatchet`, 0.95)
+
+
+--Handguns--
+SetWeaponDamageModifier(`weapon_pistol`, 0.91)
+SetWeaponDamageModifier(`weapon_pistol_mk2`, 1.03)
+SetWeaponDamageModifier(`weapon_combatpistol`, 0.92)
+SetWeaponDamageModifier(`weapon_appistol`, 0.91)
+SetWeaponDamageModifier(`weapon_stungun`, 0)
+SetWeaponDamageModifier(`weapon_pistol50`, 1.16)
+SetWeaponDamageModifier(`weapon_snspistol`, 0.95)
+SetWeaponDamageModifier(`weapon_snspistol_mk2`, 0.97)
+SetWeaponDamageModifier(`weapon_heavypistol`, 1.05)
+SetWeaponDamageModifier(`weapon_vintagepistol`, 1)
+SetWeaponDamageModifier(`weapon_flaregun`, 0.85)
+SetWeaponDamageModifier(`weapon_marksmanpistol`, 1.45)
+SetWeaponDamageModifier(`weapon_revolver`, 1.35)
+SetWeaponDamageModifier(`weapon_revolver_mk2`, 1.4)
+SetWeaponDamageModifier(`weapon_doubleaction`, 1.25)
+SetWeaponDamageModifier(`weapon_raypistol`, 0.95)
+SetWeaponDamageModifier(`weapon_ceramicpistol`, 0.97)
+SetWeaponDamageModifier(`weapon_navyrevolver`, 1.35)
+
+
+--Submachine Guns--
+SetWeaponDamageModifier(`weapon_microsmg`, 0.86)
+SetWeaponDamageModifier(`weapon_smg`, 0.87)
+SetWeaponDamageModifier(`weapon_smg_mk2`, 0.91)
+SetWeaponDamageModifier(`weapon_assaultsmg`, 0.88)
+SetWeaponDamageModifier(`weapon_combatpdw`, 0.9)
+SetWeaponDamageModifier(`weapon_machinepistol`, 0.93)
+SetWeaponDamageModifier(`weapon_minismg`, 0.87)
+SetWeaponDamageModifier(`weapon_raycarbine`, 1.2)
+
+
+--Shotguns--
+SetWeaponDamageModifier(`weapon_pumpshotgun`, 1.32)
+SetWeaponDamageModifier(`weapon_pumpshotgun_mk2`, 1.35)
+SetWeaponDamageModifier(`weapon_sawnoffshotgun`, 1.61)
+SetWeaponDamageModifier(`weapon_assaultshotgun`, 1.42)
+SetWeaponDamageModifier(`weapon_bullpupshotgun`, 1.32)
+SetWeaponDamageModifier(`weapon_musket`, 1.62)
+SetWeaponDamageModifier(`weapon_heavyshotgun`, 1.5)
+SetWeaponDamageModifier(`weapon_dbshotgun`, 1.63)
+SetWeaponDamageModifier(`weapon_autoshotgun`, 1.35)
+SetWeaponDamageModifier(`weapon_combatshotgun`,1.42)
+
+
+--Assault Rifles--
+SetWeaponDamageModifier(`weapon_assaultrifle`, 0.95)
+SetWeaponDamageModifier(`weapon_assaultrifle_mk2`, 0.98)
+SetWeaponDamageModifier(`weapon_carbinerifle`, 0.97)
+SetWeaponDamageModifier(`weapon_carbinerifle_mk2`, 1.01)
+SetWeaponDamageModifier(`weapon_advancedrifle`, 0.99)
+SetWeaponDamageModifier(`weapon_specialcarbine`, 0.99)
+SetWeaponDamageModifier(`weapon_specialcarbine_mk2`, 1.01)
+SetWeaponDamageModifier(`weapon_bullpuprifle`, 0.97)
+SetWeaponDamageModifier(`weapon_bullpuprifle_mk2`, 0.99)
+SetWeaponDamageModifier(`weapon_compactrifle`, 1.01)
+SetWeaponDamageModifier(`weapon_militaryrifle`, 1.03)
+SetWeaponDamageModifier(`weapon_heavyrifle`, 1.05)
+
+
+--Assault Rifles--
+SetWeaponDamageModifier(`weapon_mg`, 1.15)
+SetWeaponDamageModifier(`weapon_combatmg`, 1.2)
+SetWeaponDamageModifier(`weapon_combatmg_mk2`, 1.22)
+SetWeaponDamageModifier(`weapon_gusenberg`, 0.99)
+
+
+--Sniper Rifles--
+SetWeaponDamageModifier(`weapon_sniperrifle`, 1.61)
+SetWeaponDamageModifier(`weapon_heavysniper`, 1.63)
+SetWeaponDamageModifier(`weapon_heavysniper_mk2`, 1.65)
+SetWeaponDamageModifier(`weapon_marksmanrifle`, 1.35)
+SetWeaponDamageModifier(`weapon_marksmanrifle_mk2`, 1.4)
+
+
+--Heavy Weapons--
+SetWeaponDamageModifier(`weapon_rpg`, 1.65)
+SetWeaponDamageModifier(`weapon_grenadelauncher`, 1.6)
+SetWeaponDamageModifier(`weapon_grenadelauncher_smoke`,0.01)
+SetWeaponDamageModifier(`weapon_minigun`, 0.95)
+SetWeaponDamageModifier(`weapon_firework`, 1.1)
+SetWeaponDamageModifier(`weapon_railgun`, 1.55)
+SetWeaponDamageModifier(`weapon_hominglauncher`, 1.6)
+SetWeaponDamageModifier(`weapon_compactlauncher`, 1.6)
+SetWeaponDamageModifier(`weapon_rayminigun`, 0.95)
+SetWeaponDamageModifier(`weapon_emplauncher`,1.6)
+
+
+--Throwables--
+SetWeaponDamageModifier(`weapon_grenade`, 1.6)
+SetWeaponDamageModifier(`weapon_bzgas`, 0.65)
+SetWeaponDamageModifier(`weapon_molotov`, 1.15)
+SetWeaponDamageModifier(`weapon_stickybomb`, 1.6)
+SetWeaponDamageModifier(`weapon_proxmine`, 1.55)
+SetWeaponDamageModifier(`weapon_snowball`, 0.2)
+SetWeaponDamageModifier(`weapon_pipebomb`, 1.5)
+SetWeaponDamageModifier(`weapon_ball`, 0.4)
+SetWeaponDamageModifier(`weapon_smokegrenade`, 0.1)
+SetWeaponDamageModifier(`weapon_flare`, 0.1)
+
+
+--Other--
+SetWeaponDamageModifier(`weapon_hit_by_water_cannon`,0.1)
+SetWeaponDamageModifier(`weapon_fireextinguisher`, 0.1)
+
+
+end)
